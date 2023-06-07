95b71af0f91af03d7a3f253c4dbb8cfbc4d5e394
Yahaga <59613584+pzohaycuoi@users.noreply.github.com>
2023-06-07 23:13:05 +0700

    Delete authentication.py

--- report-automation/authentication.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-import subprocess
-
-
-load_dotenv()
-script_path = './report-automation-with-python/report-automation/scripts/login-az.ps1'
-output = subprocess.run(f'pwsh {script_path} -UserName {os.getenv("AZURE_USERNAME")} -Password {os.getenv("AZURE_PASSWORD")} -TenantID {os.getenv("AZURE_TENANT_ID")}', capture_output=False)
