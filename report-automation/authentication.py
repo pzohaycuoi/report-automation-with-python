f47e07480f33207fc9c603d74a89dcb89ede68dd
Yahaga <59613584+pzohaycuoi@users.noreply.github.com>
2023-06-07 23:12:49 +0700

    Update authentication.py

--- report-automation/authentication.py
+++ report-automation/authentication.py
@@ -4,5 +4,5 @@ import subprocess
 
 
 load_dotenv()
-script_path = 'C:/Users/namng/report-automation-with-python/report-automation/scripts/login-az.ps1'
+script_path = './report-automation-with-python/report-automation/scripts/login-az.ps1'
 output = subprocess.run(f'pwsh {script_path} -UserName {os.getenv("AZURE_USERNAME")} -Password {os.getenv("AZURE_PASSWORD")} -TenantID {os.getenv("AZURE_TENANT_ID")}', capture_output=False)
