cf0d36e1ee14cce286275d85bd5777ab34de073e
0x9e2fa22b <huyc26.dev@gmail.com>
2023-08-16 16:50:00 +0700

    feat: update seed sql

--- seed.sql
+++ seed.sql
@@ -38,8 +38,8 @@ CREATE TABLE db_billing_report.ea_balance_summaries
   service_overage numeric(10, 2),
   charges_billed_separately numeric(10, 2),
   total_overage numeric(10, 2),
-  total_usage numeric(10, 4),
-  azure_marketplace_service_charges numeric(10, 10),
+  total_usage numeric(10, 2),
+  azure_marketplace_service_charges numeric(10, 2),
   billing_frequency varchar(20),
   price_hidden bit,
   CONSTRAINT PK_ea_balance_summaries PRIMARY KEY (enrollment_number, date)
@@ -48,7 +48,7 @@ CREATE TABLE db_billing_report.ea_balance_summaries
 -- 🔥 Table: ea_ri_charges
 CREATE TABLE db_billing_report.ea_ri_charges
 (
-  reservation_order_name varchar(40) NOT NULL,
+  reservation_order_id varchar(50) NOT NULL,
   enrollment_number int NOT NULL,
   date varchar(8) NOT NULL,
   arm_sku_name varchar(50),
@@ -60,14 +60,14 @@ CREATE TABLE db_billing_report.ea_ri_charges
   account_owner_email varchar(50),
   cost_center varchar(50),
   event_date varchar(30),
-  reservation_order_id varchar(50),
+  reservation_order_name varchar(40),
   description varchar(100),
   event_type varchar(20),
   quantity numeric(10, 2),
   amount numeric(10, 2),
   currency varchar(10),
   billing_frequency varchar(20),
-  CONSTRAINT PK_ea_ri_charges PRIMARY KEY (reservation_order_name, enrollment_number, date),
+  CONSTRAINT PK_ea_ri_charges PRIMARY KEY (reservation_order_id),
   CONSTRAINT FK_ea_ri_charges_ea_accounts FOREIGN KEY (enrollment_number)
     REFERENCES db_billing_report.ea_accounts (enrollment_number)
 );
@@ -75,8 +75,6 @@ CREATE TABLE db_billing_report.ea_ri_charges
 -- 🔥 Table: ea_price_sheet
 CREATE TABLE db_billing_report.ea_price_sheet
 (
-  enrollment_number int NOT NULL,
-  date varchar(8) NOT NULL,
   meter_id varchar(50) NOT NULL,
   billing_period_id int,
   unit_of_measure varchar(50),
@@ -89,29 +87,27 @@ CREATE TABLE db_billing_report.ea_price_sheet
   meter_category varchar(50),
   unit varchar(20),
   meter_location varchar(max),
-  total_included_quantity numeric(15, 15),
-  pretax_standard_rate numeric(15, 15),
-  CONSTRAINT PK_ea_price_sheet PRIMARY KEY (enrollment_number, date, meter_id),
-  CONSTRAINT FK_ea_price_sheet_ea_accounts FOREIGN KEY (enrollment_number)
-    REFERENCES db_billing_report.ea_accounts (enrollment_number)
+  total_included_quantity numeric(30, 15),
+  pretax_standard_rate numeric(30, 15)
+  -- CONSTRAINT PK_ea_price_sheet PRIMARY KEY (meter_id),
 );
 
 -- 🔥 Table: ea_detail_usage
 CREATE TABLE db_billing_report.ea_detail_usage
 (
-  enrollment_number int NOT NULL,
-  date varchar(8) NOT NULL,
+  billing_account_id int NOT NULL,
+  date varchar(10) NOT NULL,
   meter_id varchar(50) NOT NULL,
   is_azure_credit_eligible bit,
-  cost numeric(15, 15),
+  cost money,
   consumed_service varchar(30),
   invoice_section varchar(50),
   account_owner_id varchar(50),
   account_name varchar(50),
   subscription_id varchar(50),
   subscription_name varchar(50),
-  quantity numeric(15, 15),
-  effective_price numeric(15, 15),
+  quantity numeric(30, 15),
+  effective_price numeric(30, 15),
   resource_location varchar(20),
   resource_id varchar(300),
   service_info_1 varchar(100),
@@ -122,9 +118,9 @@ CREATE TABLE db_billing_report.ea_detail_usage
   publisher_name varchar(100),
   tags nvarchar(max),
   additional_info nvarchar(max),
-  CONSTRAINT PK_ea_detail_usage PRIMARY KEY (enrollment_number, date, meter_id),
-  CONSTRAINT FK_ea_detail_usage_ea_accounts FOREIGN KEY (enrollment_number)
-    REFERENCES db_billing_report.ea_accounts (enrollment_number),
+  -- CONSTRAINT PK_ea_detail_usage PRIMARY KEY (billing_account_id, date, meter_id),
+  -- CONSTRAINT FK_ea_detail_usage_ea_accounts FOREIGN KEY (billing_account_id)
+  --   REFERENCES db_billing_report.ea_accounts (enrollment_number),
   -- CONSTRAINT FK_ea_detail_usage_ea_price_sheet FOREIGN KEY (meter_id)
   --   REFERENCES db_billing_report.ea_price_sheet (meter_id)
 );
